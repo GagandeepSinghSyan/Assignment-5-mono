31fa6ccd8e3b41c26ed6b161a89eb9f644e6db57
Gagandeep Singh Syan <gagandeepsinghsyan@gmail.com>
2023-06-21 13:38:22 -0400

    check Caterpillar.cs

---
+++ Caterpillar.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5_mono
+{
+    enum CaterPillarState
+    {
+        idle,
+        moving,
+        transform
+    }
+    internal class Caterpillar
+    {
+        //Caterpillar Texture
+        private Texture2D _sprite;
+
+        //position x, y and movement speed
+        private int _positionX, _positionY, _movementSpeed;
+
+        //flip sprite when required
+        private SpriteEffects _flipLeft;
+
+        //atrributes:-
+        //key input
+        //lifespan bar to make on top
+
+        public Caterpillar(int positionX, int positionY,int caterpillarSpeed, Texture2D caterpillarSprite)
+        {
+            _positionX = positionX;
+            _positionY = positionY;
+            _movementSpeed = caterpillarSpeed;
+            _sprite = caterpillarSprite;
+        }
+
+        public void Update()
+        {
+            if(Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                _positionX += 5;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿//Test Push
+﻿
 using var game = new Assignment_5_mono.Game1();
 game.Run();
